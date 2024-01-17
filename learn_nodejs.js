ae27afa966c5548d6ff11e96313baa5a7a39efc7
YUW1SI <yuw1si@bosch.com>
2024-01-17 16:27:52 +0100

    modified:   learn_nodejs.js

--- learn_nodejs.js
+++ learn_nodejs.js
@@ -1,19 +1,4 @@
-// Express application
-import express from 'express';
-// configuration
-const
-cfg = {
-port: process.env.PORT || 3000
-};
-// Express initiation
-const app = express();
-// home page route
-app.get('/', (_req, res) => {
-res.send('Hello World!');
-});
-// start server
-app.listen(cfg.port, () => {
-console.log(`Example app listening at http://localhost:${
-cfg.port }`);
-});
-
+const console = require('console');
+var foo = 'bar';
+var baz = 'trump';
+console.log('11abc');
