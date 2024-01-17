0ca96b6b662ca8bc8c607d003c6184f8fbca9c06
YUW1SI <yuw1si@bosch.com>
2024-01-17 16:33:04 +0100

    modify js

--- learn_nodejs.js
+++ learn_nodejs.js
@@ -1,4 +1,12 @@
-const console = require('console');
-var foo = 'bar';
-var baz = 'trump';
-console.log('11abc');
+// Meine Freunde als Einzelvariablen
+var freund_1 = 'Peter';
+var freund_2 = 'Paul';
+var freund_3 = 'Mary';
+// oder als Sammlung in der Form eines Arrays
+var freunde = [
+'Peter',
+'Paul',
+'Mary'
+];
+
+console.log(freunde)
